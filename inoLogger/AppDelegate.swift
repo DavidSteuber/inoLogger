048ed1dfdbc2d30dbd1f2280c9db0a6690043673
David Steuber <dsteuber@gmail.com>
2016-01-19 23:51:58 -0500

    Initial Commits
    Created Xcode project. Moved files. Commit project template.

---
+++ inoLogger/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  inoLogger
+//
+//  Created by David Steuber on 1/19/16.
+//  Copyright © 2016 David Steuber.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ inoLogger/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  inoLogger
+//
+//  Created by David Steuber on 1/19/16.
+//  Copyright © 2016 David Steuber.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
